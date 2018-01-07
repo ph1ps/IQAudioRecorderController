f52dc0c814f45db3a110508a23b37a35ca9a3e33
ph1ps <ph1psdev@gmail.com>
2018-01-07 12:46:36 +0100

    Add umbrella header for Swift compilation

--- IQAudioRecorderController/IQAudioRecorderController.h
+++ IQAudioRecorderController/IQAudioRecorderController.h
@@ -16,4 +16,15 @@ FOUNDATION_EXPORT const unsigned char IQAudioRecorderControllerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <IQAudioRecorderController/PublicHeader.h>
 
-#import "IQAudioRecorderViewController.h"
+#import <IQAudioRecorderController/IQAudioRecorderViewController.h>
+#import <IQAudioRecorderController/IQAudioCropperViewController.h>
+#import <IQAudioRecorderController/IQAudioRecorderController.h>
+#import <IQAudioRecorderController/IQMessageDisplayView.h>
+#import <IQAudioRecorderController/IQ_FDWaveformView.h>
+#import <IQAudioRecorderController/NSString+IQTimeIntervalFormatter.h>
+#import <IQAudioRecorderController/IQCropSelectionBeginView.h>
+#import <IQAudioRecorderController/IQCropSelectionEndView.h>
+#import <IQAudioRecorderController/IQCropSelectionView.h>
+#import <IQAudioRecorderController/SCSiriWaveformView.h>
+#import <IQAudioRecorderController/IQPlaybackDurationView.h>
+
